f24e2f29a9af7636879df39a5fd4a7b42cd56cd8
jlnzhou <30112251+jlnzhou@users.noreply.github.com>
2022-05-30 13:54:07 +0200

    Update loader parameters

--- src/loader.py
+++ src/loader.py
@@ -1,6 +1,7 @@
 import os
 import sys
 from jax.config import config
+import numpy as np
 # Agents
 from src.agent.kernel_ucb import KernelUCB
 # Kernels
@@ -14,24 +15,32 @@ base_dir = os.path.dirname(os.path.realpath(__file__))
 sys.path.append(base_dir)
 
 
-def get_agent_by_name(settings):
-    if settings['agent'] == 'k_ucb':
+def get_agent_by_name(parameters):
+    if parameters['agent'] == 'k_ucb':
         return KernelUCB
     else:
         raise NotImplementedError
 
 
-def get_env_by_name(settings):
-    if settings['env'] == 'bump':
-        return EnvBump
+def get_env_by_name(parameters):
+    if parameters['env'] == 'bump':
+        return EnvBump(rd_seed=parameters['random_seed'],
+                       horizon=parameters['T'],
+                       actions=np.linspace(parameters['min_action'], parameters["max_action"], parameters["n_actions"]),
+                       contexts=np.linspace(parameters['min_context'], parameters["max_context"], parameters["n_contexts"]),
+                       actions_dim=parameters["dim_actions"],
+                       contexts_dim=parameters["dim_contexts"],
+                       noise_scale=parameters["noise_scale"])
     else:
         raise NotImplementedError
 
 
-def get_kernel_by_name(settings):
-    if settings['kernel'] == 'gauss':
-        return Gaussian
-    elif settings['kernel'] == 'exp':
-        return Exponential
+def get_kernel_by_name(parameters):
+    if parameters['kernel'] == 'gauss':
+        return Gaussian(parameters,
+                        gaussian_scale=parameters['kernel_param'])
+    elif parameters['kernel'] == 'exp':
+        return Exponential(parameters,
+                           exp_scale=parameters['kernel_params'])
     else:
         raise NotImplementedError
