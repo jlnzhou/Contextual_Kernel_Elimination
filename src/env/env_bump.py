baaede4cd39fb4ba0958f4f19792703e15508f93
jlnzhou <30112251+jlnzhou@users.noreply.github.com>
2022-05-30 16:04:22 +0200

    Update bump

--- src/env/env_bump.py
+++ src/env/env_bump.py
@@ -32,19 +32,20 @@ class EnvBump:
         self.w_star = self.env_rng.choice(a=self.contexts,
                                           size=self.contexts_dim)
 
+    def sample_data(self):
+        return self.env_rng.choice(a=self.contexts,
+                                   size=self.contexts_dim)
+
     def sample_reward(self, state):
-        context, action = state[:, self.contexts_dim], state[:, self.contexts_dim:]
+        context, action = state[:, :self.contexts_dim], state[:, self.contexts_dim:]
         term = np.linalg.norm(action - self.a_star, ord=1) + np.dot(context - self.x_star, self.w_star).squeeze()
         r = max(0, 1 - term)
         return np.array([r])
 
     def sample_reward_noisy(self, state):
-        return [self.sample_reward(state) + self.env_rng.normal(loc=0.0, scale=self.noise_scale)]
-
-    def find_best_input_in_joint_space(self, joint_pair):
-        return np.argmax(self.sample_reward(joint_pair))
+        return self.sample_reward(state) + self.env_rng.normal(loc=0.0, scale=self.noise_scale)
 
     def get_best_reward_in_context(self, context):
         term = np.dot(context - self.x_star, self.w_star).squeeze()
         r = max(0, 1 - term)
-        return r
+        return np.array([r])
