6550fcf8d8b017c1d3227c8497121a2dfaa5e02a
jlnzhou <30112251+jlnzhou@users.noreply.github.com>
2022-05-31 13:26:52 +0200

    Adds jax in bump

--- src/env/env_bump.py
+++ src/env/env_bump.py
@@ -1,5 +1,5 @@
 import numpy as np
-
+import jax.numpy as jnp
 
 class EnvBump:
 
@@ -37,14 +37,14 @@ class EnvBump:
 
     def sample_reward(self, state):
         context, action = state[:, :self.contexts_dim], state[:, self.contexts_dim:]
-        term = np.linalg.norm(action - self.a_star, ord=1) + np.dot(context - self.x_star, self.w_star).squeeze()
+        term = jnp.linalg.norm(action - self.a_star, ord=1) + jnp.dot(context - self.x_star, self.w_star).squeeze()
         r = max(0, 1 - term)
-        return np.array([r])
+        return jnp.array([r])
 
     def sample_reward_noisy(self, state):
         return self.sample_reward(state) + self.env_rng.normal(loc=0.0, scale=self.noise_scale)
 
     def get_best_reward_in_context(self, context):
-        term = np.dot(context - self.x_star, self.w_star).squeeze()
+        term = jnp.dot(context - self.x_star, self.w_star).squeeze()
         r = max(0, 1 - term)
-        return np.array([r])
+        return jnp.array([r])
